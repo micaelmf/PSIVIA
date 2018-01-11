acbd54d9536910985ebd69cc43ac0bf70b8a5fb2
Micael Ferreira <micaelmf@gmail.com>
2018-01-11 19:49:53 -0300

    Teste para a resposta cachorro (refactor)

--- PSIVIA-Teste/src/Teste/Teste.java
+++ PSIVIA-Teste/src/Teste/Teste.java
@@ -9,6 +9,8 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+import DAO.AnimalDAO;
+import DAO.PerguntaDAO;
 import Dominio.Animal;
 import Dominio.Pergunta;
 import Dominio.Resposta;
@@ -18,26 +20,25 @@ class Teste {
 	@Test
 	void esperaRespostaCachorro() {
 		Resposta resposta = new Resposta();
-		ArrayList<Pergunta> perguntas = new ArrayList<Pergunta>();
-		Pergunta p1 = new Pergunta("Tem rabo?");
-		perguntas.add(p1);
-		Pergunta p2 = new Pergunta("Tem 4 patas?");
-		perguntas.add(p2);
-		Pergunta p3 = new Pergunta("Tem pêlos?");
-		perguntas.add(p3);
-		Pergunta p4 = new Pergunta("Ele late?");
-		perguntas.add(p4);
-		Pergunta p5 = new Pergunta("Ele mia?");
-		perguntas.add(p5);
+		
+		AnimalDAO daoAnimal = new AnimalDAO();
+		PerguntaDAO daoPergunta = new PerguntaDAO();
+		
+		ArrayList<Pergunta> perguntas = daoPergunta.carregaPerguntas();
+		Animal a = new Animal("Cachorro");
+		Animal animal = daoAnimal.carregaAnimal(a);
+		Map<String,Boolean> atributosDoAnimal = new LinkedHashMap<>();
+		atributosDoAnimal = animal.getAtributos();
 		
 		Map<String,Boolean> atributos = new LinkedHashMap<>();
-		atributos.put(p1.getPergunta(), true);
-		atributos.put(p2.getPergunta(), true);
-		atributos.put(p3.getPergunta(), true);
-		atributos.put(p4.getPergunta(), true);
-		atributos.put(p5.getPergunta(), false);
+		for(Pergunta p : perguntas) {
+			if(atributosDoAnimal.containsKey(p.getPergunta())) {
+				atributos.put(p.getPergunta(), true);
+			}else {
+				atributos.put(p.getPergunta(), false);
+			}
+		}
 		
-		Animal animal = new Animal("Cachorro");
 		resposta.setProcurar(animal.getNome());
 		resposta.setResposta(atributos);
 		String retorno = resposta.getResposta();   
@@ -77,6 +78,9 @@ class Teste {
 	@Test
 	void retornaNenhumAnimal() {
 		Resposta resposta = new Resposta();
+
+		PerguntaDAO daoPergunta = new PerguntaDAO();
+		
 		ArrayList<Pergunta> perguntas = new ArrayList<Pergunta>();
 		Pergunta p1 = new Pergunta("Tem rabo?");
 		perguntas.add(p1);
