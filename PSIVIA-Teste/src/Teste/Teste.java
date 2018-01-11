619dc0ed151232a337cc64e7a206b9cf4190ba4c
Micael Ferreira <micaelmf@gmail.com>
2018-01-11 19:50:43 -0300

    Teste para a resposta gato (refactor)

--- PSIVIA-Teste/src/Teste/Teste.java
+++ PSIVIA-Teste/src/Teste/Teste.java
@@ -48,30 +48,29 @@ class Teste {
 	
 	@Test
 	void esperaRespostaGato() {
-		Resposta resposta = new Resposta();
-		ArrayList<Pergunta> perguntas = new ArrayList<Pergunta>();
-		Pergunta p1 = new Pergunta("Tem rabo?");
-		perguntas.add(p1);
-		Pergunta p2 = new Pergunta("Tem 4 patas?");
-		perguntas.add(p2);
-		Pergunta p3 = new Pergunta("Tem pêlos?");
-		perguntas.add(p3);
-		Pergunta p4 = new Pergunta("Ele late?");
-		perguntas.add(p4);
-		Pergunta p5 = new Pergunta("Ele mia?");
-		perguntas.add(p5);
+Resposta resposta = new Resposta();
+		
+		AnimalDAO daoAnimal = new AnimalDAO();
+		PerguntaDAO daoPergunta = new PerguntaDAO();
+		
+		ArrayList<Pergunta> perguntas = daoPergunta.carregaPerguntas();
+		Animal a = new Animal("Gato");
+		Animal animal = daoAnimal.carregaAnimal(a);
+		Map<String,Boolean> atributosDoAnimal = new LinkedHashMap<>();
+		atributosDoAnimal = animal.getAtributos();
 		
 		Map<String,Boolean> atributos = new LinkedHashMap<>();
-		atributos.put(p1.getPergunta(), true);
-		atributos.put(p2.getPergunta(), true);
-		atributos.put(p3.getPergunta(), true);
-		atributos.put(p4.getPergunta(), false);
-		atributos.put(p5.getPergunta(), true);
+		for(Pergunta p : perguntas) {
+			if(atributosDoAnimal.containsKey(p.getPergunta())) {
+				atributos.put(p.getPergunta(), true);
+			}else {
+				atributos.put(p.getPergunta(), false);
+			}
+		}
 		
-		Animal animal = new Animal("Gato");
 		resposta.setProcurar(animal.getNome());
 		resposta.setResposta(atributos);
-		String retorno = resposta.getResposta();  
+		String retorno = resposta.getResposta();   
 		
 		assertEquals(animal.getNome(), retorno);
 	}
