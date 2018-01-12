2c3a5a35c2c6671906255ecdf4cc4df596f97ee2
Micael Ferreira <micaelmf@gmail.com>
2018-01-11 22:06:06 -0300

    Teste insere nova pergunta para animal diferente do procurado (green)

--- PSIVIA-Teste/src/Teste/Teste.java
+++ PSIVIA-Teste/src/Teste/Teste.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
@@ -112,7 +113,7 @@ class Teste {
 		PerguntaDAO daoPergunta = new PerguntaDAO();
 		
 		ArrayList<Pergunta> perguntas = daoPergunta.carregaPerguntas();
-		Animal a = new Animal("G");
+		Animal a = new Animal("diferente");
 		Animal animal = daoAnimal.carregaAnimal(a);
 		Map<String,Boolean> atributosDoAnimal = new LinkedHashMap<>();
 		atributosDoAnimal = animal.getAtributos();
@@ -131,5 +132,47 @@ class Teste {
 		String retorno = resposta.getResposta();   
 		
 		assertEquals("Duiker-zebrado", retorno);
+	}
+	
+	@Test
+	void insereNovaPerguntaParaAnimalDiferenteDoProcurado() {
+		Resposta resposta = new Resposta();
+		
+		AnimalDAO daoAnimal = new AnimalDAO();
+		PerguntaDAO daoPergunta = new PerguntaDAO();
+		
+		ArrayList<Pergunta> perguntas = daoPergunta.carregaPerguntas();
+		Random random = new Random();
+		
+		int numAleatorio = random.nextInt();
+		String codigo = Integer.toString(numAleatorio) ;
+		
+		Animal a = new Animal(codigo);
+		Animal animal = daoAnimal.carregaAnimal(a);
+		Map<String,Boolean> atributosDoAnimal = new LinkedHashMap<>();
+		atributosDoAnimal = animal.getAtributos();
+		
+		Map<String,Boolean> atributos = new LinkedHashMap<>();
+		for(Pergunta p : perguntas) {
+			if(atributosDoAnimal.containsKey(p.getPergunta())) {
+				atributos.put(p.getPergunta(), true);
+			}else {
+				atributos.put(p.getPergunta(), false);
+			}
+		}
+		
+		resposta.setProcurar(animal.getNome());
+		resposta.setResposta(atributos);
+		String retorno = resposta.getResposta();
+		
+		boolean resultado = false;
+		Map<String,Boolean> novosAtributos = new LinkedHashMap<>();
+		if(retorno != a.getNome()) {
+			novosAtributos.put(codigo, true);
+			Animal novoAnimal = new Animal(a.getNome(),novosAtributos);
+			resultado = daoAnimal.gravarAnimal(novoAnimal);
+		}
+		
+		assertEquals(true, resultado);
 	}
 }
